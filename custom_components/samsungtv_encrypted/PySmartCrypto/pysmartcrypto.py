f0bb1b55e1e46ffb60c8f22848133b2f2fb7591c
Matteo Agnoletto <acc.epmatt@gmail.com>
2020-12-03 13:41:24 +0100

    Hardcoded ports in step4 and websocket connections (#66)
    * revert to hardcoded ports
    
    * remove commented print statement

--- custom_components/samsungtv_encrypted/PySmartCrypto/pysmartcrypto.py
+++ custom_components/samsungtv_encrypted/PySmartCrypto/pysmartcrypto.py
@@ -91,10 +91,9 @@ class PySmartCrypto():
 
     def connect(self):
         millis = int(round(time.time() * 1000))
-        step4_url = 'http://' + self._host + ':'+self._port+'/socket.io/1/?t=' + str(millis)
+        step4_url = 'http://' + self._host + ':8000/socket.io/1/?t=' + str(millis)
         websocket_response = requests.get(step4_url)
-        websocket_url = 'ws://' + self._host + ':'+self._port+'/socket.io/1/websocket/' + websocket_response.text.split(':')[0]
-        # print(websocket_url)
+        websocket_url = 'ws://' + self._host + ':8000/socket.io/1/websocket/' + websocket_response.text.split(':')[0]
         # pairs to this app with this command.
         connection = websocket.create_connection(websocket_url)
         connection.send('1::/com.samsung.companion')
